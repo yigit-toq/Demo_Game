be7509aeab07b8db11ae0266bad4adcebc58d585
Yiğit Top <yigit.toq@gmail.com>
2024-10-13 03:01:20 +0300

    Minister Economy

---
+++ Assets/Script/GameMechanical.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMechanical : MonoBehaviour
+{
+    public static UI_Kod kodUI;
+
+    public GameObject sacilanPara;
+
+    private GameObject klonObje;
+
+    private Vector2 fareKonum;
+
+    public float elimizdekiCoin;
+    public float kazanilanCoin;
+    public float verilenCoin;
+    public float hedefCoin;
+
+    void Start()
+    {
+        kodUI = FindObjectOfType<UI_Kod>();
+
+        verilenCoin = 1f;
+        hedefCoin = 50f;
+    }
+
+    void Update()
+    {
+        Coin_Kazanma ();
+
+        elimizdekiCoin = kazanilanCoin;
+    }
+
+    void Coin_Kazanma ()
+    {
+        if (Input.GetMouseButtonDown(0) && !kodUI.yükselt)
+        {
+            kazanilanCoin += verilenCoin;
+            fareKonum = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            klonObje = Instantiate(sacilanPara, fareKonum, Quaternion.identity);
+            Destroy(klonObje, 2f);
+        }
+    }
+}
---
+++ Assets/Script/UI_Kod.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UI_Kod : MonoBehaviour
+{
+    public static GameMechanical gameMec;
+
+    public Text coinText;
+    public Text hedefText;
+    public Text verilenCoin;
+
+    public bool yükselt;
+
+    public float coinUI;
+
+    public int birler;
+    public int onlar;
+    public int yüzler;
+    public int binler;
+
+    void Start ()
+    {
+        gameMec = FindObjectOfType<GameMechanical>();
+
+        yükselt = false;
+
+        hedefText.text = "HEDEF" + ":" + gameMec.hedefCoin.ToString();
+        verilenCoin.text = "x" + gameMec.verilenCoin.ToString();
+    }
+
+    void Update ()
+    {
+        coinUI = gameMec.elimizdekiCoin;
+
+        coinText.text = coinUI.ToString();
+    }
+
+    public void Upgrade_Button ()
+    {
+        if (coinUI >= gameMec.hedefCoin)
+        {
+            yükselt = true;
+            gameMec.kazanilanCoin -= gameMec.hedefCoin;
+            gameMec.verilenCoin = gameMec.verilenCoin * 2;
+            gameMec.hedefCoin = gameMec.hedefCoin * 2;
+            verilenCoin.text = "x" + gameMec.verilenCoin.ToString();
+            hedefText.text = "HEDEF " + ": " + gameMec.hedefCoin.ToString();
+        }
+
+        yükselt = false;
+    }
+}
